869aeca7ca45d62d9ccc44348228c5ff84d05616
Zena Hirsch <zena@vimeo.com>
2021-06-12 11:25:20 -0400

    Add stock-watcher-lifx

---
+++ stock-watcher-lifx.py
@@ -0,0 +1,50 @@
+import ally
+from lights import charizard
+from colors import DIM_RED, DIM_BLUE, DIM_CYAN, DIM_GREEN, BRIGHT_RED
+
+SYMBOL = 'vmeo'
+
+
+def update_light(error=False, bid_price=0, open_price=0):
+    if error:
+        charizard.set_color(DIM_RED)
+        charizard.set_waveform(is_transient=False, color=BRIGHT_RED, period=1000, cycles=5, duty_cycle=1,
+                               waveform=1)
+        return
+
+    if bid_price == 0:  # outside market hours
+        charizard.set_color(DIM_CYAN)
+        return
+
+    if bid_price > open_price:
+        charizard.set_color(DIM_GREEN)
+    elif bid_price < open_price:
+        charizard.set_color(DIM_RED)
+    else:
+        charizard.set_color(DIM_BLUE)
+
+
+def main():
+    a = ally.Ally()
+
+    charizard.set_power('on')
+    charizard.set_color(DIM_CYAN)
+
+    quotes = a.quote(symbols=SYMBOL, fields=['opn', 'bid'], dataframe=False)
+
+    open_price = float(quotes[0]['opn'])
+    bid_price = float(quotes[0]['bid'])
+
+    update_light(bid_price=bid_price, open_price=open_price)
+
+    try:
+        for quote in a.stream(SYMBOL):
+            bid_price = float(quote['bid'])
+            update_light(bid_price=bid_price, open_price=open_price)
+    except:
+        update_light(error=True)
+        raise
+
+
+if __name__ == '__main__':
+    main()
