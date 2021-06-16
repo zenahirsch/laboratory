a4d8e052a0a0929525277a1eae74000524a57f77
Zena Hirsch <hirsch.zena@gmail.com>
2021-06-15 20:04:22 -0400

    Add dim purple and dim orange

--- test_colors.py
+++ test_colors.py
@@ -3,7 +3,7 @@ from colors import BRIGHT_RED, DIM_ORANGE
 
 def test_color(light, color):
     print(f"Testing color {color} on light {light}")
-    charizard.set_color(color)
+    light.set_color(color)
 
 if __name__ == '__main__':
     test_color(charizard, DIM_ORANGE)
