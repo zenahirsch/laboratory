50c6593a9a07576c01fbd12d3035bcbd82ef7c11
Zena Hirsch <zena@vimeo.com>
2021-06-15 22:35:32 -0400

    Add Pikachu to lights

--- lights.py
+++ lights.py
@@ -1,11 +1,12 @@
 from lifxlan import LifxLAN
 
-NUM_LIGHTS = 3
+NUM_LIGHTS = 4
 
 lan = LifxLAN(NUM_LIGHTS)
 
 charizard = lan.get_device_by_name('Charizard')
 bulbasaur = lan.get_device_by_name('Bulbasaur')
 blastoise = lan.get_device_by_name('Blastoise')
+pikachu = lan.get_device_by_name('Pikachu')
 
 laboratory = lan.get_devices_by_group("Laboratory")
